d858e6c3d3f2f3f9cccb744e5e8a1c3c09cd4c3f
endritvs <endritsaiti8@gmail.com>
2024-01-19 22:14:44 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import Vue from '@vitejs/plugin-vue';
+import path from 'path'; // Import the 'path' module
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    Vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
