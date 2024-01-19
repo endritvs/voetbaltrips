e23cd50d0b073f88db07673ed66546d7a97712db
endritvs <endritsaiti8@gmail.com>
2024-01-19 21:42:16 +0100

    EndritS-update-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './index.html', './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
   theme: {
     container: {
       center: true,
       screens: {
         sm: '100%',    
         lg: '960px',
-        xl: '1080px'  
+        xl: '1100px'  
       },
     },
     extend: {},
